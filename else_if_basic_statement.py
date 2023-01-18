64210e4f88c58dd740d91878974f61f05db315c2
Manoj Chandran <73032685+manojc31@users.noreply.github.com>
2023-01-18 16:11:17 -0600

    Add files via upload

---
+++ else_if_basic_statement.py
@@ -0,0 +1,8 @@
+def dave_check(user_name):
+    if user_name == "mynameis":
+        return "I know it's lora"
+    elif user_name == "queen31":
+        return "I know it's you Dave"
+
+
+print(dave_check(user_name = "queen31"))
