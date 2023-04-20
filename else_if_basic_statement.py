1386a7615842b3c019a362478f31a0692e215c2a
Manoj Chandran <73032685+manojc31@users.noreply.github.com>
2023-04-20 14:01:37 -0500

    Delete else_if_basic_statement.py

--- else_if_basic_statement.py
+++
@@ -1,8 +0,0 @@
-def dave_check(user_name):
-    if user_name == "mynameis":
-        return "I know it's lora"
-    elif user_name == "queen31":
-        return "I know it's you Dave"
-
-
-print(dave_check(user_name = "queen31"))
